c2081a179c1446da0a7192f87f083f322970c6c6
Curtis Brown <cwbro251@learner.dvusd.org>
2021-12-08 11:00:49 -0700

    Added The Code For The Binary to Decimal

--- app/src/main/java/com/example/bin2dec/MainActivity.kt
+++ app/src/main/java/com/example/bin2dec/MainActivity.kt
@@ -9,4 +9,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
     }
+    fun convertBinaryToDecimal(num: Long): Int {
+        var num = num
+        var decimalNumber = 0
+        var i = 0
+        var remainder: Long
+
+        while (num.toInt() != 0) {
+            remainder = num % 10
+            num /= 10
+            decimalNumber += (remainder * Math.pow(2.0, i.toDouble())).toInt()
+            ++i
+        }
+        return decimalNumber
+    }
 }
